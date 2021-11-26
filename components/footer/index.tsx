658b5c9868ced8a76c8705d359bd3b1d2e4449ab
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2021-11-26 08:32:30 +0100

    Update footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.scss'
 
 const Footer: NextComponentType = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles['footer']}>
       <p>&copy; Oleksii Shevtsov</p>
     </footer>
   )
